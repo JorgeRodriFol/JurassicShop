41f6bd49627ba0b07eb689d8abb707440ce43f11
Jorge Rodriguez Folguera <83008485+JorgeRodriFol@users.noreply.github.com>
2024-09-29 10:23:46 +0200

    Menu

--- php/visual/index.php
+++ php/visual/index.php
@@ -17,8 +17,20 @@
             <div class="menu">
                 <ul>
                     <li><a href="#ofertas">Ofertas</a></li>
-                    <li><a href="#jurassic-park">Jurassic Park</a></li>
-                    <li><a href="#walking-with">Walking With</a></li>
+                    <li><a href="#jurassic-park">Jurassic Park</a>
+                        <ul>
+                            <li><a href="#jp1">Jurassic Park</a></li>
+                            <li><a href="jp2">Jurassic Park: The Lost World</a></li>
+                            <li><a href="jp3">Jurassic Park III</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#walking-with">Walking With</a>
+                        <ul>
+                            <li><a href="#jp1">Walking With Monsters</a></li>
+                            <li><a href="jp2">Walking With Dinosaurs</a></li>
+                            <li><a href="jp3">Walking With Beast</a></li>
+                        </ul>
+                    </li>
                     <li><a href="#dinosaur-revolution">Dinosaur Revolution</a></li>
                     <li><a href="#mas-especies">Más especies</a></li>
                     <li><a href="#tienda-fisica">Tienda Física</a></li>
