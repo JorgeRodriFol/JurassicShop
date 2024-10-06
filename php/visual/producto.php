a62f8cae6d4bc965fa1a923c5499ee5ad5110d13
Jorge Rodriguez Folguera <83008485+JorgeRodriFol@users.noreply.github.com>
2024-10-06 09:56:54 +0200

    a

--- php/visual/producto.php
+++ php/visual/producto.php
@@ -5,10 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Início</title>
-    <link rel="stylesheet" href="../../css/indexStyle.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 
 <body>
@@ -30,15 +27,15 @@
                     <li><a href="#jurassic-park">Jurassic Park</a>
                         <ul>
                             <li><a href="#jp1">Jurassic Park</a></li>
-                            <li><a href="#jp2">Jurassic Park: The Lost World</a></li>
-                            <li><a href="#jp3">Jurassic Park III</a></li>
+                            <li><a href="jp2">Jurassic Park: The Lost World</a></li>
+                            <li><a href="jp3">Jurassic Park III</a></li>
                         </ul>
                     </li>
                     <li><a href="#walking-with">Walking With</a>
                         <ul>
-                            <li><a href="#WWM">Walking With Monsters</a></li>
-                            <li><a href="#WWD">Walking With Dinosaurs</a></li>
-                            <li><a href="#WWB">Walking With Beast</a></li>
+                            <li><a href="#jp1">Walking With Monsters</a></li>
+                            <li><a href="jp2">Walking With Dinosaurs</a></li>
+                            <li><a href="jp3">Walking With Beast</a></li>
                         </ul>
                     </li>
                     <li><a href="#dinosaur-revolution">Dinosaur Revolution</a></li>
@@ -54,28 +51,7 @@
             </div>
         </div>
         <div class="body">
-            <div class="categoria">
-                <h2>NOVEDADES</h2>
-                <div class="carrusel" id="novedadescarrusel">
-                    <img class="left" src="../../img/assets/ic--round-play-arrow.png" alt="">
-                    <div class="productos" id="novedades">
-                        <div class="producto">
-                            <a href="#rexypop">
-                                <img src="../../img/productos/rexypop.png" alt="">
-                                <div class="info">
-                                    <p class="titulo">Funko POP Rexy</p>
-                                    <p class="precio">15.00€</p>
-                                    <div class="comprar">
-                                        <input type="text" id="cantidad" value="1">
-                                        <button>COMPRAR</button>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <img class="right" src="../../img/assets/ic--round-play-arrow.png" alt="">
-                </div>
-            </div>
+
         </div>
     </div>
 </body>
